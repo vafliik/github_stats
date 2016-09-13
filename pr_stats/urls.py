25cc21f418b2552bdc93dd5aa68e4c65d90cdf51
Pavel Pribyl <pribyl.pavel@gmail.com>
2016-09-13 23:57:09 +0200

    Simple week traversing

--- pr_stats/urls.py
+++ pr_stats/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pr_number>[0-9]+)/$', views.detail, name='detail'),
     url(r'^pulls$', views.pulls, name='pulls'),
-    url(r'^statistics', views.statistics, name='statistics'),
+    url(r'^statistics/(?P<year>\d{4})/(?P<month>\d{0,2})/(?P<day>\d+)/$', views.statistics, name='statistics'),
+    url(r'^statistics', views.statistics, name='statistics-basic'),
 ]
--- pr_stats/views.py
+++ pr_stats/views.py
@@ -2,19 +2,23 @@ import logging
 
 import dateutil.parser
 
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timezone, timedelta, date
+
+import django
 from django.db.models import Min, F, Max, Avg
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.utils import timezone
 
 from pr_stats import services
 from pr_stats.models import PullRequest, Event, User
 from pr_stats.services import median_value, create_user_if_not_already, get_all_pulls, update_pulls
 
 
-
 def index(request):
-    pr_list = PullRequest.objects.order_by('-number')
+    today = date.today()
+    this_monday = today - timedelta(days=today.weekday())
+
+    pr_list = PullRequest.objects.filter(created_at__gte=this_monday).order_by('-number')
     context = {
         'pr_list': pr_list,
     }
@@ -29,14 +33,34 @@ def detail(request, pr_number):
     return render(request, 'pr_stats/detail.html', context)
 
 
-def statistics(request):
-    pulls = PullRequest.objects.filter(state='closed')
+def statistics(request, year=None, month=None, day=None):
+    if year:
+        year = int(year)
+        month = int(month)
+        day = int(day)
+        start = django.utils.timezone.now().replace(year=year, month=month, day=day, hour=0, minute=0, second=0, microsecond=0)
+    else:
+        today = django.utils.timezone.now()
+        this_monday = (today - timedelta(days=today.weekday())).replace(hour=0, minute=0, second=0, microsecond=0)
+        start = this_monday
+
+    end = (start + timedelta(days=7)).replace(hour=23, minute=59, second=59, microsecond=99)
+
+    previous = start - timedelta(days=7)
+    next = start + timedelta(days=7)
+
+    query_filter = {'state': 'closed', 'created_at__gte': start, 'created_at__lt': end}
+
+    pulls = PullRequest.objects.filter(**query_filter)
     fastest_pr = pulls.order_by('closed_after_sec')[:1]
     slowest_pr = pulls.order_by('-closed_after_sec')[:1]
     average_time = pulls.aggregate(Avg('closed_after_sec'))['closed_after_sec__avg']
     median_time = median_value(pulls, 'closed_after_sec')
     context = {
         'pulls': pulls,
+        'query_filter': query_filter,
+        'previous': previous,
+        'next': next,
         'fastest_pr': fastest_pr[0],
         'slowest_pr': slowest_pr[0],
         'average_time': timedelta(seconds=average_time),
@@ -46,7 +70,6 @@ def statistics(request):
 
 
 def pulls(request):
-
     last_update = PullRequest.objects.aggregate(Max('updated_at'))['updated_at__max']
     if last_update is not None:
         buffer_time = last_update - timedelta(minutes=15)
@@ -57,4 +80,3 @@ def pulls(request):
         update_pulls()
 
     return redirect('pr_stats:index')
-
