342db71e66b5e0c95400981c4d917f5636322625
Pavel Pribyl <pavelp@salsitasoft.com>
2016-09-14 16:08:56 +0200

    Unlimited connection

--- github_stats/settings.py
+++ github_stats/settings.py
@@ -88,7 +88,7 @@ except KeyError:
     pass
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=None)
 DATABASES['default'].update(db_from_env)
 
 
